fd92b22db1b154faa8cd285b440a4b09daebf9d8
Chotosaurio <agustinespin@hotmail.com>
2016-06-12 15:57:49 -0300

    Update conflictosentrereinas.cpp

--- conflictosentrereinas.cpp
+++ conflictosentrereinas.cpp
@@ -2,124 +2,35 @@
 #include<fstream>
 using namespace std;
 ifstream entra;
-ofstream sale;//N y M n= tamaño del tablero m= numero de reinas[con f para filas en su definicion][y con c para columna] 
+ofstream sale;//N y M n= tamaÃ±o del tablero m= numero de reinas[con f para filas en su definicion][y con c para columna] 
 //salida mostrar en orden las reinas si otra reina las bloquea en cualquier direcion y con cuales(mostrar su nro)
 struct posicion{
 	int f,c;
 };
-void reinasvermas(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
+void reinasvermas(int f, int c, int fmas, int cmasint conflictoint, list<posicion>Auxiliardeconflictos){
 	posicion conflictostruc;
 	for(int j=1;j<=n;j++){
 				conflictostruc.f=f;
 				conflictostruc.c=c;
-				if (table[f+j][c]=3{
+				if (table[f+(j*fmas)][c+(j*cmas)]=3{
 					conflictoint++;
 					Auxiliardeconflictos.push(conflictostruc)
 					break;}
-				if(f+j>n)
+				if(f+(j*fmas)>n || f+(j*fmas)<1 || c+(j*cmas)>n || c+(c*fmas)<1)
 				break;
 		}
 }
-void reinasvermenos(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f-j][c]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(f-j<1)
-				break;
-		}
-}
-void reinashormas(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f][c+j]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(c+j>n)
-				break;
-		}
-}
-void reinashormenos(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f][c-j]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(c-j<1)
-				break;
-		}
-}
-void reinasdiagdermas(int f, int c, int conflictoint,list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f+j][c+j]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(f+j>n or c+j>n)
-				break;
-		}
-}
-void reinasdiadermenos(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f+j][c-j]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(f+j>n or c-j<1)
-				break;
-		}
-}
-void reinasdiagizmas(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f-j][c+j]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(f-j<1 or c+j>n)
-				break;
-		}
-}
-void reinasdiagizmenos(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	posicion conflictostruc;
-	for(int j=1;j<=n;j++){
-				conflictostruc.f=f;
-				conflictostruc.c=c;
-				if (table[f-j][c-j]=3{
-					conflictoint++;
-					Auxiliardeconflictos.push(conflictostruc)
-					break;}
-				if(f+j<1 or c+j<1)
-				break;
-		}
-}
-void resolucion(int f, int c, int conflictoint, list<posicion>Auxiliardeconflictos){
-	reinavermas(f,c,conflictoint,Auxiliardeconflictos)
-	reinavermenos(f,c,conflictoint,Auxiliardeconflictos)
-	reinahormas(f,c,conflictoint,Auxiliardeconflictos)
-	reinahormenos(f,c,conflictoint,Auxiliardeconflictos)
-	reinadiagdermas(f,c,conflictoint,Auxiliardeconflictos)
-	reinadiagdermenos(f,c,conflictoint,Auxiliardeconflictos)
-	reinadiagizmas(f,c,conflictoint,Auxiliardeconflictos)
-	reinadiagizmenos(f,c,conflictoint,Auxiliardeconflictos)
+
+void resolucion(int f, int c, list<posicion>Auxiliardeconflictos){
+	reinamover(f,c,1,0,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,-1,0,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,0,1,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,0,-1,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,1,1,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,-1,-1,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,1,-1,conflictoint,Auxiliardeconflictos)
+	reinamover(f,c,-1,1,conflictoint,Auxiliardeconflictos)
+
 	
 }
 int main(){
@@ -129,6 +40,7 @@ int main(){
 	for(int i=1;i<=m;i++){
 		entra>>f>>c;
 		table[f][c]=3;
+	//otro for con la resolucion del ejercicio y l identificacion de cada reina(con su numero de entrada)
 	}
 	
 }
